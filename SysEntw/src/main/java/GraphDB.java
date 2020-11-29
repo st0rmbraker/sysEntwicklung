c7b2de6026eb4c1b2adbb5bccba0e7d8f22ae1bf
st0rmbraker <lennart.m.hergel@gmail.com>
2020-11-29 14:35:32 +0100

    stonks - works now

--- SysEntw/src/main/java/GraphDB.java
+++ SysEntw/src/main/java/GraphDB.java
@@ -1,6 +1,8 @@
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.record.OElement;
 
 public class GraphDB {
 
@@ -9,8 +11,28 @@ public class GraphDB {
 
         ODatabaseSession db = orient.open("VehicleHistoryGraph","root","123456");
 
+        OElement element = db.newInstance("Person");
+        element.setProperty("name", "John");
+
+//at this stage the record is not yet persistent
+
+        System.out.println(element.getIdentity()); //this will print a temporary RID (negative cluster position)
+
+        element.save();
+
+//the element is now persistent
+
+        System.out.println(element.getIdentity()); //this will print the valid, final RID for that document.
+        ORID temp = element.getIdentity();
+
+
+        OElement doc = db.load(temp);
+        System.out.println(doc.getProperty("name"));
+
         db.close();
         orient.close();
+
+        }
     }
 
-}
+
