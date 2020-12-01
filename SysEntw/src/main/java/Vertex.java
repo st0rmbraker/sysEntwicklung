8df26a41c29887b7c9d8a61a15d58b1f5550c1bb
GetAlexed0 <71035944+GetAlexed0@users.noreply.github.com>
2020-12-01 20:29:30 +0100

    Ich hasse mein Leben

--- SysEntw/src/main/java/Vertex.java
+++ SysEntw/src/main/java/Vertex.java
@@ -1,24 +1,16 @@
 import com.orientechnologies.orient.core.db.ODatabaseSession;
 import com.orientechnologies.orient.core.db.OrientDB;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
-import com.orientechnologies.orient.core.record.OVertex;
+
 
 public class Vertex {
 
     public static void main(String[] args) {
         OrientDB orient = new OrientDB("remote:localhost", OrientDBConfig.defaultConfig());
 
-        ODatabaseSession db = orient.open("DHBWDB","root","123456");
+        //FICK DOCH MEIN LEBEN GRAPHDB IST DER GRÖßTE BS
+        Vertex v = null;
 
-        db.createVertexClass("Person");
-
-        OVertex person1 = db.newVertex("Person");
-        OVertex person2 = db.newVertex("Person");
-
-
-
-
-        db.close();
         orient.close();
     }
 
