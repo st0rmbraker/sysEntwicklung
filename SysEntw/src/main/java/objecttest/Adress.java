70262049553d40cd8c41488438d3465597854556
GetAlexed0 <71035944+GetAlexed0@users.noreply.github.com>
2020-12-22 12:48:09 +0100

    Object Database added

---
+++ SysEntw/src/main/java/objecttest/Adress.java
@@ -0,0 +1,28 @@
+package objecttest;
+
+public class Adress {
+
+    private String name;
+    private City city;
+    private String string;
+    private String street;
+
+    public Adress(String string, City city, String street)
+    {
+        this.street =street;
+        this.city = city;
+        this.string = string;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void add(Adress residence) {
+
+    }
+}
---
+++ SysEntw/src/main/java/objecttest/City.java
@@ -0,0 +1,29 @@
+package objecttest;
+
+public class City {
+
+    String name;
+    Country country;
+
+    public City(String name, Country country)
+    {
+        this.name = name;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
