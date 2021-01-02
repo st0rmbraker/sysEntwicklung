13f4997d972e8489970e755968d9788bed4ace3e
DETIHOE <s195109@student.dhbw-mannheim.de>
2021-01-02 17:42:19 +0100

    Klasse GUIT

---
+++ SysEntw/src/main/java/GUIT.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUIT {
+    private JEditorPane hiermitStimmeIchDenEditorPane;
+    private JCheckBox testCheckBox;
+    private JButton einverstandenButton;
+    private JScrollPane scrollPane1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
