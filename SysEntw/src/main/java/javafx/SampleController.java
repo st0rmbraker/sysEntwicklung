dc939dabc589928d5dbf6e0d27252dec5b4be9e0
MaFi2000 <75029494+MaFi2000@users.noreply.github.com>
2021-01-03 19:01:39 +0100

    fx testen

---
+++ SysEntw/src/main/java/javafx/SampleController.java
@@ -0,0 +1,14 @@
+package javafx;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController {
+
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
