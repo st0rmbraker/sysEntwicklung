97f0f66cd9c1dd1ddc2682863034bf3ec857becd
GetAlexed0 <71035944+GetAlexed0@users.noreply.github.com>
2021-01-03 12:43:21 +0100

    added javafx test

--- SysEntw/src/main/java/javafx/SceneTest.java
+++ SysEntw/src/main/java/javafx/SceneTest.java
@@ -1,8 +1,23 @@
 package javafx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class SceneTest  {
+public class SceneTest extends Application {
 
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sceneTest.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
