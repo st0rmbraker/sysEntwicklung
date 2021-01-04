b2b7c129031f160dbf439501e1e25a78bed1898e
GetAlexed0 <71035944+GetAlexed0@users.noreply.github.com>
2021-01-04 20:09:52 +0100

    for the noobs

--- SysEntw/src/main/java/javafx/Main.java
+++ SysEntw/src/main/java/javafx/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root;
-        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
