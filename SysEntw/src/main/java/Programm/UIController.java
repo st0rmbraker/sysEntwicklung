11875437188ff79f1fa748cc36b8a8cb118db61d
lenimanuela <74671698+lenimanuela@users.noreply.github.com>
2021-01-05 20:01:14 +0100

    layout

--- SysEntw/src/main/java/Programm/UIController.java
+++ SysEntw/src/main/java/Programm/UIController.java
@@ -1,24 +1,28 @@
 package Programm;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextFlow;
 
 public class UIController {
 
-    public Label helloWorld;
-    public Button button2;
-    public Button hanswurst;
-    public SplitPane SplitPane_1;
+    //public Label helloWorld;
+    public Button follows_me;
+    public ImageView profile_image;
+    public MenuButton profile_settings;
+    public ImageView follower_image;
+    public ListView follower_details;
+    public TextFlow chat;
+    public Button profile_following;
+    public ListView examples;
+    public TextField insert_user;
 
-    public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
-    }
+    //public void sayHelloWorld(ActionEvent actionEvent) {helloWorld.setText("Hello World!");}
 
-    public void testbuitton(ActionEvent actionEvent) {
-        hanswurst.setText("hhh");
-    }
+    public void follows_me(ActionEvent actionEvent) { follows_me.getId(); }
+
 
 
 }
