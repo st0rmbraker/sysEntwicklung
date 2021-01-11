184ebf315d007191a3285f095e61aa96db6c8439
st0rmbraker <lennart.m.hergel@gmail.com>
2021-01-11 16:06:28 +0100

    hoffentlich alles wieder Rückgängig

--- SysEntw/src/main/java/Programm/UIController.java
+++ SysEntw/src/main/java/Programm/UIController.java
@@ -1,21 +1,42 @@
 package Programm;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.TextFlow;
 
 public class UIController {
 
-    public Label helloWorld;
-    public Button button2;
-    public Button hanswurst;
+    //public Label helloWorld;
+    public Button follows_me;
+    public ImageView profile_image;
+    public MenuButton profile_settings;
+    public ImageView follower_image;
+    public ListView follower_details;
+    public TextFlow chat;
+    public Button profile_following;
+    public ListView examples;
+    public TextField insert_user;
 
-    public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
+    Helpers h = new Helpers();
+
+    //public void sayHelloWorld(ActionEvent actionEvent) {helloWorld.setText("Hello World!");}
+
+    public void follows_me_action(ActionEvent actionEvent) {
+
+        System.out.println("Test");
+        h.session();
+        insert_user.setText(h.outputQueryAcc("SELECT FROM ACCOUNT"));
     }
 
-    public void testbuitton(ActionEvent actionEvent) {
-        hanswurst.setText("hhh");
-    }
+
+    //lollll
+    //lllooooollll
+    //lalala
+
+
+
+
 }
 
