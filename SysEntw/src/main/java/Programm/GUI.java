e96cc2a216c3945f77c19c529f57ce914a5643ec
st0rmbraker <lennart.m.hergel@gmail.com>
2021-01-11 17:55:15 +0100

    getUser()

--- SysEntw/src/main/java/Programm/GUI.java
+++ SysEntw/src/main/java/Programm/GUI.java
@@ -6,9 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 import static javafx.fxml.FXMLLoader.load;
 
+
+
 public class GUI extends Application {
+
+    String user;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,11 +22,24 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        user = JOptionPane.showInputDialog(null,
+                "Bitte geben sie ihren Benutzernamen ein.",
+                JOptionPane.DEFAULT_OPTION);
+
         Parent root;
         root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
+    public String getUser(){
+        return user;
+    }
+
+
+
+
 
 }
