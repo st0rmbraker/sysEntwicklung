a5ee33efd5096d8c4525fbeed7bf02e8ac16ebe4
st0rmbraker <lennart.m.hergel@gmail.com>
2021-01-12 23:45:05 +0100

    Es wird

--- SysEntw/src/main/java/Programm/UIController.java
+++ SysEntw/src/main/java/Programm/UIController.java
@@ -1,7 +1,9 @@
 package Programm;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
 import com.orientechnologies.orient.core.record.ODirection;
 import com.orientechnologies.orient.core.record.OVertex;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -40,10 +42,11 @@ public class UIController extends Helpers {
     public TextArea own_infos;
     public TextArea to_follow_infos;
     public TextArea output_chat;
+    public Button change_info;
 
     Helpers h = new Helpers();
     OVertex user;
-
+    Dialog <String[]> dialog;
 
 
     //Der "+"-Button. Aktuell angemeldeter User folgt dem in dem Textfeld "insert_user" eingegeben Benutzernamen, wenn vorhanden.
@@ -62,73 +65,66 @@ public class UIController extends Helpers {
             this.user = h.getVertexByUsername(user);
         }
         else{
-            // Eigenen JavaFX Dialog zum Nutzer erstellen
-            Dialog <String[]> dialog = new Dialog<>();
-            dialog.setTitle("Neuen Nutzer erstellen");
-            dialog.setHeaderText("Neuen Nutzer erstellen");
-
-            //dialog.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
-
-            // Set the button types.
-            ButtonType create = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
-            dialog.getDialogPane().getButtonTypes().addAll(create, ButtonType.CANCEL);
-
-            GridPane grid = new GridPane();
-            grid.setHgap(10);
-            grid.setVgap(10);
-            grid.setPadding(new Insets(20, 150, 10, 10));
-
-            TextField nFirstName = new TextField();
-            nFirstName.setPromptText("Vorname");
-            TextField nLastName = new TextField();
-            nLastName.setPromptText("Nachname");
-            TextField nUsername = new TextField();
-            nUsername.setPromptText("Benutzername");
-
-            grid.add(new Label("Vorname:"), 0, 0);
-            grid.add(nFirstName, 1, 0);
-            grid.add(new Label("Nachname:"), 0, 1);
-            grid.add(nLastName, 1, 1);
-            grid.add(new Label("Benutzername:"), 0, 2);
-            grid.add(nUsername, 1, 2);
-
-            // Enable/Disable login button depending on whether a username was entered.
-            Node loginButton = dialog.getDialogPane().lookupButton(create);
-            loginButton.setDisable(true);
-
-            // Do some validation (using the Java 8 lambda syntax).
-            nUsername.textProperty().addListener((observable, oldValue, newValue) -> {
-                loginButton.setDisable(newValue.trim().isEmpty());
-            });
-
-            dialog.getDialogPane().setContent(grid);
-
-            // Request focus on the username field by default.
-            Platform.runLater(() -> nFirstName.requestFocus());
-            nFirstName.getText();
-            // Convert the result to a username-password-pair when the login button is clicked.
-            dialog.setResultConverter(dialogButton -> {
-                if (dialogButton == create) {
-                    return new String[]{nFirstName.getText(), nLastName.getText(), nUsername.getText()};
-                }
-                return null;
-            });
-
+            prepareCreateUser();
             Optional<String[]> result = dialog.showAndWait();
 
-           /*
-            TextInputDialog dialog = new TextInputDialog("");
-            dialog.setTitle("Benutzername eingeben");
-            dialog.setHeaderText("Bitte geben sie ihren Benutzernamen ein.");
-            dialog.setContentText("Bitte Namen eingeben:");
-            Optional<String> userC = dialog.showAndWait();
-            user = userC.get();
-*/
             createUser(result.get()[2], result.get()[0], result.get()[1]);
         }
-
     }
 
+    public void prepareCreateUser(){
+        // Eigenen JavaFX Dialog zum Nutzer erstellen
+        dialog = new Dialog<>();
+        dialog.setTitle("Neuen Nutzer erstellen");
+        dialog.setHeaderText("Neuen Nutzer erstellen");
+
+        //dialog.setGraphic(new ImageView(this.getClass().getResource("login.png").toString()));
+
+        // Set the button types.
+        ButtonType create = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(create, ButtonType.CANCEL);
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField nFirstName = new TextField();
+        nFirstName.setPromptText("Vorname");
+        TextField nLastName = new TextField();
+        nLastName.setPromptText("Nachname");
+        TextField nUsername = new TextField();
+        nUsername.setPromptText("Benutzername");
+
+        grid.add(new Label("Vorname:"), 0, 0);
+        grid.add(nFirstName, 1, 0);
+        grid.add(new Label("Nachname:"), 0, 1);
+        grid.add(nLastName, 1, 1);
+        grid.add(new Label("Benutzername:"), 0, 2);
+        grid.add(nUsername, 1, 2);
+
+        // Enable/Disable login button depending on whether a username was entered.
+        Node loginButton = dialog.getDialogPane().lookupButton(create);
+        loginButton.setDisable(true);
+
+        // Do some validation (using the Java 8 lambda syntax).
+        nUsername.textProperty().addListener((observable, oldValue, newValue) -> {
+            loginButton.setDisable(newValue.trim().isEmpty());
+        });
+
+        dialog.getDialogPane().setContent(grid);
+
+        // Request focus on the username field by default.
+        Platform.runLater(() -> nFirstName.requestFocus());
+        nFirstName.getText();
+        // Convert the result to a username-password-pair when the login button is clicked.
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == create) {
+                return new String[]{nFirstName.getText(), nLastName.getText(), nUsername.getText()};
+            }
+            return null;
+        });
+    }
 
     //Stellt Liste der Follower in Richtung Rein oder Raus bereit
     public ObservableList<String> prepareFollowers(OVertex element, String direction){
@@ -156,6 +152,29 @@ public class UIController extends Helpers {
     }
     */
 
+    public void onClick_change_info(){
+        System.out.println("onClick_change_info");
+        if(user.getProperty("userInfos")==null){
+            TextInputDialog dialog = new TextInputDialog("Hier können sie weitere Informationen hinzufügen");
+            dialog.setTitle("User Infos hinzufügen");
+            dialog.setHeaderText("Look, a Text Input Dialog");
+            dialog.setContentText("Please enter your name:");
+            Optional<String> result = dialog.showAndWait();
+            if (result.isPresent()){
+                System.out.println("Your name: " + result.get());
+                ODocument doc = new ODocument("userInfos");
+                doc.field("name", result.get());
+                doc.save();
+
+            }
+        }
+        else{
+            ODatabaseDocument info = user.getProperty("userInfos");
+        }
+
+
+    }
+
     public void onClick_following_button(ActionEvent actionEvent) {
         System.out.println("onClick_following_button");
         output_follower.setItems(prepareFollowers(user, "OUT"));
@@ -165,7 +184,6 @@ public class UIController extends Helpers {
         System.out.println("onClick_me_following_button");
         output_follower.setItems(prepareFollowers(user, "IN"));
     }
-
 
     //Der "+"-Button. Aktuell angemeldeter User folgt dem in dem Textfeld "insert_user" eingegeben Benutzernamen, wenn vorhanden.
     public void onClick_to_follow_button(ActionEvent event) {
